542448f760ac87e494fed8a05b23de16c8546a43
FakhriNuril <fakhri.marekan@gmail.com>
2026-09-24 14:32:13 +0700

    fakhri battle
    BattleSeason

---
+++ BattleSeason/Program.cs
@@ -0,0 +1,18 @@
+﻿string nama;
+
+int TotalEXP;
+
+Console.Write("Input character's name:");
+Console.ReadLine();
+Console.Write("Input the number of battles:");
+int number = int.Parse (Console.ReadLine());
+Console.Write("Input the exp gained per battles:");
+int Exp = int .Parse(Console.ReadLine());
+
+
+//work
+
+for  (int Total = 1; Total<= number; Total++ );
+{
+    if ()
+}
